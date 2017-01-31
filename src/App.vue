38c942c3e6f5d8636901d02eeacfbafdf0cc37d1
infernalslam <infernal-slam@hotmail.com>
2017-02-01 04:15:26 +0700

    :sleepy:

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,19 @@
       </div>
 
       <div class="footer">
+        <div class="star"> <!-- Place this tag where you want the button to render. -->
+          <!-- Place this tag where you want the button to render. -->
+          <a class="github-button" href="https://github.com/infernalslam" data-style="mega" data-count-href="/infernalslam/followers" data-count-api="/users/infernalslam#followers" data-count-aria-label="# followers on GitHub" aria-label="Follow @infernalslam on GitHub">Follow @infernalslam</a>
+          <a class="github-button" href="https://github.com/infernalslam/vuejs-symbol-iot" data-style="mega" aria-label="Star infernalslam/vuejs-symbol-iot on GitHub">Star</a>
+          <!-- Place this tag where you want the button to render. -->
+          <a class="github-button" href="https://github.com/infernalslam/vuejs-symbol-iot/issues" data-style="mega" data-count-api="/repos/infernalslam/vuejs-symbol-iot#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue infernalslam/vuejs-symbol-iot on GitHub">Issue</a>
+        </div>
         <div class="footer-description2">
         Built with <a href="https://github.com/vuejs/vue" target="_blank" class="footer-link-description">Vuejs</a>
         <img src="../static/Vuejs.png" class="footer-logo" alt="Vue Logo">
+        And
+        <a href="https://github.com/surmon-china/vue-codemirror" target="_blank" class="footer-link-description">Vuejs</a>
+        <img src="../static/code.png" style="width: 31.99px; height: 31.99px;"class="footer-logo" alt="Vue Logo">
        </div>
      </div>
 </template>
@@ -48,18 +58,33 @@ export default {
 }
 body {
   font-family: "San Francisco";
+  background: rgba(255,255,255,1);
+background: -moz-linear-gradient(left, rgba(255,255,255,1) 0%, rgba(246,246,246,1) 42%, rgba(189,182,189,1) 91%, rgba(189,182,189,1) 100%);
+background: -webkit-gradient(left top, right top, color-stop(0%, rgba(255,255,255,1)), color-stop(42%, rgba(246,246,246,1)), color-stop(91%, rgba(189,182,189,1)), color-stop(100%, rgba(189,182,189,1)));
+background: -webkit-linear-gradient(left, rgba(255,255,255,1) 0%, rgba(246,246,246,1) 42%, rgba(189,182,189,1) 91%, rgba(189,182,189,1) 100%);
+background: -o-linear-gradient(left, rgba(255,255,255,1) 0%, rgba(246,246,246,1) 42%, rgba(189,182,189,1) 91%, rgba(189,182,189,1) 100%);
+background: -ms-linear-gradient(left, rgba(255,255,255,1) 0%, rgba(246,246,246,1) 42%, rgba(189,182,189,1) 91%, rgba(189,182,189,1) 100%);
+background: linear-gradient(to right, rgba(255,255,255,1) 0%, rgba(246,246,246,1) 42%, rgba(189,182,189,1) 91%, rgba(189,182,189,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#bdb6bd', GradientType=1 );
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /*text-align: center;*/
-  color: #2c3e50;
+  /*color: #2c3e50;*/
   margin-top: 60px;
 }
 
 .box-char.green {
-    background: #81C784;
+  background: rgba(210,255,82,1);
+background: -moz-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(114,194,39,1) 42%, rgba(114,194,39,1) 100%);
+background: -webkit-gradient(left top, right top, color-stop(0%, rgba(210,255,82,1)), color-stop(42%, rgba(114,194,39,1)), color-stop(100%, rgba(114,194,39,1)));
+background: -webkit-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(114,194,39,1) 42%, rgba(114,194,39,1) 100%);
+background: -o-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(114,194,39,1) 42%, rgba(114,194,39,1) 100%);
+background: -ms-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(114,194,39,1) 42%, rgba(114,194,39,1) 100%);
+background: linear-gradient(to right, rgba(210,255,82,1) 0%, rgba(114,194,39,1) 42%, rgba(114,194,39,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#d2ff52', endColorstr='#72c227', GradientType=1 );
     text-align: center;
 }
 
@@ -106,4 +131,12 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#06f', endCol
   border: 1px #000000 solid;
 }
 
+.star {
+  position: absolute;
+left: 40%;
+bottom: 124%;
+right: 0;
+margin-left: auto;
+margin-right: auto;
+}
 </style>
